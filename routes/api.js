2d3a5277fddbf326cfe222fa25e91ef025fb766d
Unknown <mrcrozier@gmail.com>
2017-11-10 10:10:41 -0500

    Added ability to update by coasterId

--- routes/api.js
+++ routes/api.js
@@ -1,40 +1,49 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 const Coaster = require('../models/coaster');
 
 // get a list of coasters from the db
-router.get('/coasters', function(req, res, next){
-    Coaster.find({}).then(function(coasters){
-      res.send(coasters);
+router.get('/coasters', function(req, res, next) {
+    Coaster.find({}).then(function(coasters) {
+        res.send(coasters);
     });
 });
 
-router.get('/coasters/:id', function(req, res, next){
-    Coaster.find({}).then(function(coasters){
-      res.send(coasters);
+router.get('/coasters/:id', function(req, res, next) {
+    Coaster.find({}).then(function(coasters) {
+        res.send(coasters);
     });
 });
 
 
 // add a new coaster to the db
-router.post('/coasters', function(req, res, next){
-    Coaster.create(req.body).then(function(coaster){
+router.post('/coasters', function(req, res, next) {
+    Coaster.create(req.body).then(function(coaster) {
         res.send(coaster);
     }).catch(next);
 });
 
 // update a coaster in the db
-router.put('/coasters/:id', function(req, res, next){
-    Coaster.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
-        Coaster.findOne({_id: req.params.id}).then(function(coaster){
+router.put('/coasters/:id', function(req, res, next) {
+    Coaster.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
+        Coaster.findOne({ _id: req.params.id }).then(function(coaster) {
+            res.send(coaster);
+        });
+    }).catch(next);
+});
+
+//update a coaster by its coaster id instead of its generated mongoDb _id
+router.put('/coasters/update/:coasterId', function(req, res, next) {
+    Coaster.findOneAndUpdate({ coasterId: req.params.coasterId }, req.body).then(function() {
+        Coaster.findOne({ coasterId: req.params.coasterId }).then(function(coaster) {
             res.send(coaster);
         });
     }).catch(next);
 });
 
 // delete a coaster from the db
-router.delete('/coasters/:id', function(req, res, next){
-    Coaster.findByIdAndRemove({_id: req.params.id}).then(function(coaster){
+router.delete('/coasters/:id', function(req, res, next) {
+    Coaster.findByIdAndRemove({ _id: req.params.id }).then(function(coaster) {
         res.send(coaster);
     }).catch(next);
 });
